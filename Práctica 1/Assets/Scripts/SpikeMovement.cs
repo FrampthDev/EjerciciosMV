591156eb005402e36386094deb15c7eacce49f1b
link10804k <62154757+link10804k@users.noreply.github.com>
2024-10-14 12:30:17 +0200

    Más cambios

--- Práctica 1/Assets/Scripts/SpikeMovement.cs
+++ Práctica 1/Assets/Scripts/SpikeMovement.cs
@@ -1,21 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Xml.Serialization;
 using UnityEngine;
 
-public class ScriptSpike : MonoBehaviour
+public class SpikeMovement : MonoBehaviour
+
 {
     public float speed = 5f;
     public float alturaMax = 4.5f;
-    
-    public GameObject bubble;
-    public static extern Object Instantiate(Object original);
-
-    //public GameObject spikeGameObject;
     // Start is called before the first frame update
     void Start()
     {
-      
+        
     }
 
     // Update is called once per frame
@@ -25,10 +20,5 @@ public class ScriptSpike : MonoBehaviour
 
         if (transform.position.y >= alturaMax)
             Destroy(gameObject);
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Destroy(collision.gameObject);
-        Object.Instantiate(bubble, transform.position, transform.rotation);
     }
 }
