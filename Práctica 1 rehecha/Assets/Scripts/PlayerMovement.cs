5d4e4b52fefbda30cd763ee6a81ed412c95c61b4
link10804k <62154757+link10804k@users.noreply.github.com>
2024-10-21 11:30:18 +0200

    1º cambio Práctica 1 rehecha
    Configuración de la cámara, creación de la escena, creación del jugador

---
+++ Práctica 1 rehecha/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float Speed = 5f;
+    [SerializeField] private float MaxLeft = -7.25f, MaxRight = 7.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.A)) && (transform.position.x > MaxLeft))
+            transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        if ((Input.GetKey(KeyCode.D)) && (transform.position.x < MaxRight))
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+    }
+}
